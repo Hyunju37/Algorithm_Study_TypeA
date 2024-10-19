cfe1022525b668563cd157ff75e70eae67a10cc8
hj <charmsxda402@gmail.com>
2024-10-19 21:54:40 +0900

    add: 241019 prgms_144854 [조건에 맞는 도서와 저자 리스트 출력하기] Solved

---
+++ Hyunju37/week_9/sql/144854.sql
@@ -0,0 +1,9 @@
+SELECT 
+    b.book_id AS book_id,
+    a.author_name AS author_name,
+    DATE_FORMAT(b.published_date, '%Y-%m-%d') AS published_date
+FROM book b
+INNER JOIN 
+    author a ON b.author_id = a.author_id
+WHERE b.category = '경제'
+ORDER BY b.published_date ASC;
