4b874c1d3ec3a1e817dc9c8f0c9e031e12fd9268
hj <charmsxda402@gmail.com>
2024-10-20 16:09:53 +0900

    fix: 241019 prgms_293257 [물고기 종류 별 잡은 수 구하기] Solved

--- Hyunju37/week_9/sql/293257.sql
+++ Hyunju37/week_9/sql/293257.sql
@@ -4,5 +4,7 @@ SELECT
 FROM fish_info i
 INNER JOIN 
     fish_name_info n ON i.fish_type = n.fish_type
-GROUP BY i.fish_type, n.fish_name
+GROUP BY 
+    i.fish_type, 
+    n.fish_name
 ORDER BY fish_count DESC;
