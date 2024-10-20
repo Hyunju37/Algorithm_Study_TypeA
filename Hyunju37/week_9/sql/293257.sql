87b431a56305e96236871e2a333248a23b87a037
hj <charmsxda402@gmail.com>
2024-10-20 15:22:17 +0900

    add: 241019 prgms_293257 [물고기 별 잡은 수 구하기] Solved

---
+++ Hyunju37/week_9/sql/293257.sql
@@ -0,0 +1,8 @@
+SELECT 
+    COUNT(*) AS fish_count,
+    n.fish_name AS fish_name
+FROM fish_info i
+INNER JOIN 
+    fish_name_info n ON i.fish_type = n.fish_type
+GROUP BY i.fish_type, n.fish_name
+ORDER BY fish_count DESC;
