cfbfa3fb6b4b262f74de729a4e06946258a19d55
hj <charmsxda402@gmail.com>
2024-10-21 12:48:33 +0900

    add: 241021 prgms_299308 [분기별 분화된 대장균의 개체 수 구하기] Failed

--- Hyunju37/week_9/sql/299308.sql
+++ Hyunju37/week_9/sql/299308.sql
@@ -1,8 +1,6 @@
-SELECT 
-    COUNT(*) as fish_count
-FROM fish_info
-WHERE fish_type IN (
-    SELECT fish_type
-    FROM fish_name_info
-    WHERE fish_name in ('BASS', 'SNAPPER')
-);
+SELECT
+    CONCAT(FLOOR(MONTH(differentiation_date) / 4) + 1, 'Q') AS quarter,
+    COUNT(*) AS ecoli_count
+FROM ecoli_data
+GROUP BY quarter
+ORDER BY quarter ASC;
