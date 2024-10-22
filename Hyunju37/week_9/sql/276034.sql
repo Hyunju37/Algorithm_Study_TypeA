870b0e779eded61126b8e85bb9e8311afde54148
hj <charmsxda402@gmail.com>
2024-10-22 13:47:32 +0900

    add: 241021 prgms_276034 [조건에 맞는 개발차 찾기] Solved

---
+++ Hyunju37/week_9/sql/276034.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT
+    d.id,
+    d.email, 
+    d.first_name,
+    d.last_name
+FROM 
+    developers d
+JOIN 
+    skillcodes s ON s.name in ('Python', 'C#')
+WHERE d.skill_code & s.code = s.code
+ORDER BY d.id ASC;
