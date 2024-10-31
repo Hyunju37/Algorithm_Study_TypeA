dd6be298afa0140ae7fc7bd17aff97b7cf47dcf1
hj <charmsxda402@gmail.com>
2024-10-31 16:14:08 +0900

    fix: 241031 prgms_299308 [분기별 분화된 대장균의 개체 수 구하기] Solved

--- Hyunju37/week_9/sql/299308.sql
+++ Hyunju37/week_9/sql/299308.sql
@@ -1,5 +1,10 @@
 SELECT
-    CONCAT(FLOOR(MONTH(differentiation_date) / 4) + 1, 'Q') AS quarter,
+    CASE
+        WHEN MONTH(differentiation_date) <= 3 THEN '1Q'
+        WHEN MONTH(differentiation_date) <= 6 THEN '2Q'
+        WHEN MONTH(differentiation_date) <= 9 THEN '3Q'
+        ELSE '4Q'
+    END AS quarter,
     COUNT(*) AS ecoli_count
 FROM ecoli_data
 GROUP BY quarter
