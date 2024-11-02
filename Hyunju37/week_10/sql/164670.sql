86950726831dd5425fe0dd5b34f41caf8d21947c
hj <charmsxda402@gmail.com>
2024-11-02 21:26:26 +0900

    add: 241102 prgms_164670 [조건에 맞는 사용자 정보 조회하기] Solved

---
+++ Hyunju37/week_10/sql/164670.sql
@@ -0,0 +1,14 @@
+SELECT 
+    u.user_id,
+    u.nickname,
+    CONCAT(u.city, ' ', u.street_address1, ' ', u.street_address2) AS '전체주소',
+    CONCAT(SUBSTRING(u.tlno, 1, 3), '-',
+          SUBSTRING(u.tlno, 4, 4), '-',
+          SUBSTRING(u.tlno, 8, 4)) AS '전화번호'
+FROM used_goods_user u
+INNER JOIN 
+    used_goods_board b ON b.writer_id = u.user_id
+GROUP BY u.user_id
+HAVING COUNT(*) >= 3
+ORDER BY u.user_id DESC;
+
