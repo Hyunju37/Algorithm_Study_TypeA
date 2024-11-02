f63bb2ac62c5e13b8585e5d041e5b60ba69c4fe5
hj <charmsxda402@gmail.com>
2024-11-02 21:34:02 +0900

    add: 241102 prgms_284529 [부서별 평균 연봉 조회하기] Solved

---
+++ Hyunju37/week_10/sql/284529.sql
@@ -0,0 +1,9 @@
+SELECT
+    d.dept_id,
+    d.dept_name_en,
+    ROUND(AVG(e.sal), 0) AS avg_sal
+FROM hr_department d
+JOIN 
+    hr_employees e ON d.dept_id = e.dept_id
+GROUP BY d.dept_id
+ORDER BY avg_sal DESC;
